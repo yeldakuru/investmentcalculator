fb48d21e209478df4e004b0bad5767ba2abb8939
Yelda Kuru <yeldakuru99@outlook.com>
2026-07-26 22:08:51 +0300

    handling events and using two-way binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Header from './components/Header'
-import UserInput from './components/UserInput'
+import Header from './components/Header.jsx'
+import UserInput from './components/UserInput.jsx'
 
 function App() {
   return (
-    <Header />
-    <UserInput/>
-  )
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
 export default App
